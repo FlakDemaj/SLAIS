9a55651e01adee34128c79bf40ee0ccd5ecc789c
Flakron Demaj <flakron.demaj@student.uni-siegen.de>
2026-03-20 17:30:18 +0100

    SLAIS-1: Refactor Methods

--- Domain/System/RefreshToken/RefreshTokenErrorCodes.cs
+++ Domain/System/RefreshToken/RefreshTokenErrorCodes.cs
@@ -5,11 +5,11 @@ namespace Domain.Systems.RefreshToken;
 public enum RefreshTokenErrorCodes
 {
     [Description("Irgendwas lief falsch. Bitte versuchen Sie es bald erneut.")]
-    InvalidExpiration = -400001,
+    InvalidExpiration = -410001,
 
     [Description("Irgendwas lief falsch. Bitte versuchen Sie es bald erneut.")]
-    InvalidDeviceGuid = -400002,
+    InvalidDeviceGuid = -410002,
 
     [Description("Irgendwas lief falsch. Bitte versuchen Sie es bald erneut.")]
-    InvalidDeviceName = -400003,
+    InvalidDeviceName = -410003,
 }
