f0dd3596b5a1250ee0ed216708b587b4013a446d
FlakDemaj <152999680+FlakDemaj@users.noreply.github.com>
2026-03-27 15:48:38 +0100

    Change RoleConstants access modifier to internal

--- Presentation/Utils/Role.cs
+++ Presentation/Utils/Role.cs
@@ -4,15 +4,14 @@ namespace Presentation.Utils;
 
 public class Role
 {
-    protected static class RoleConstants
+    internal static class RoleConstants
     {
-        protected const string Server = nameof(Roles.Server);
+        internal const string Server = nameof(Roles.Server);
 
-        protected const string Admin = nameof(Roles.Admin);
+        internal const string Admin = nameof(Roles.Admin);
 
-        protected const string Teacher = nameof(Roles.Teacher);
+        internal const string Teacher = nameof(Roles.Teacher);
 
-        protected const string Student = nameof(Roles.Student);
-
+        internal const string Student = nameof(Roles.Student);
     }
 }
